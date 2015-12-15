88ab620fa65a5bd50803d560000993d765e172ea
maryallisonabad <maryallisonabad@gmail.com>
2015-12-15 17:21:09 -0500

    Rearranged firework.h and added function in firework.cpp

--- firework.cpp
+++ firework.cpp
@@ -2,37 +2,54 @@
 #include "firework.h"
 
 const GLfloat Firework::Gravity = 0.05f;
-int lastUsedParticle = 0;
+Firework firework;
 
-int findUnusedParticle() {
-    for ( int i = lastUsedParticle; i < Max_Particles; i++ ) {
-        if ( FireworkContainer[i].life < 0 ) {
-            lastUsedParticle = i;
-            return i;
-        }
+void Firework::initParticles() {
+    float xInitLoc = rand() % 500;
+    //Random speeds, not sure of values
+    float initParticleSpeed = rand() % 100;
+    float initParticleAngle = rand() % 100 + 45;
+
+    // Assign rbg colors, 0-255 randomly. Alpha set to max for now
+    QColor initColor;
+    initColor.setRed(rand() % 255);
+    initColor.setGreen(rand() % 255);
+    initColor.setBlue(rand() % 255);
+    initColor.setAlpha(255);
+
+    // Initialize all particles to have the same xLoc, speed, angle, and color
+    for (std::vector<Particle>::iterator it = firework.particles.begin() ; it != firework.particles.end(); ++it) {
+        Particle particle = *it;
+        particle.xLoc = xInitLoc;
+        particle.velocity.speed = initParticleSpeed;
+        particle.velocity.angle = initParticleAngle;
+        particle.color = initColor;
     }
-
-    for ( int i = 0; i < lastUsedParticle; i++ ) {
-        if ( FireworkContainer[i].life < 0 ) {
-            lastUsedParticle = i;
-            return i;
-        }
-    }
-
-    return 0; // Reset first particle
 }
 
 void Firework::burst() {
     //Code for firework exploding
-    for ( int i = 0; i < 123; i++ ) {
-        //TODO: Slowdown particle (to stop), use gravity, replace 123 w/ num particles
-
+    for (std::vector<Particle>::iterator it = firework.particles.begin() ; it != firework.particles.end(); ++it) {
+        float particleSpeed = rand() % 25;
+        float particleAngle = rand() % 100 + 45;
+        Particle particle = *it;
+        particle.velocity.speed = particleSpeed;
+        particle.velocity.angle = particleAngle;
     }
     if ( alpha > 0.0f ) {
         alpha -= 0.01f; //Make firework particle invisible
     }
+    miniLaunch();
 }
 
 void Firework::launch() {
     //Shoots the firework
+
+
+    //At apex of arc, burst
+    burst();
+}
+
+void Firework::miniLaunch() {
+
 }
--- firework.h
+++ firework.h
@@ -11,42 +11,23 @@ struct Velocity {
 
 struct Particle {
     float mass;
+    Velocity velocity;
+    QColor color;
+    float xLoc; //Initial X Location;
 };
 
-const int Max_Particles = 10000;
-Firework FireworkContainer[Max_Particles];
+const int Max_Particles = 100;
+//Firework FireworkContainer[Max_Particles];
 
 struct Firework {
-    Firework() {
-        //Generating random x (y stays the same)
-        xLoc = ((float)rand() / (float)RAND_MAX) * 800.0f; // TODO: Play with float value
-        //Random speeds, not sure of values
-        x.speed = rand() % 100;
-        y.speed = rand() % 100;
-
-        // Assign rng colors, 0-255 randomly. Alpha set to max for now
-        color.setRed(rand() % 255);
-        color.setGreen(rand() % 255);
-        color.setBlue(rand() % 255);
-        color.setAlpha(255);
-
-        hasExploded = false; //Since firework just started
-
-        cout << "Firework created" << endl;
-    }
-    GLfloat Gravity;
+    GLfloat gravity;
     float radius;
     vector<Particle> particles;
-    Velocity x, y;
-    //pair<float> position;
-    QColor color; // Made this a QColor instead of a QRgb
 
-    float xLoc; //Initial X Location;
-    float life; //Remaining life of particle (when should it die)?
-    bool hasExploded;
-
+    void initParticles();
     void burst();
     void launch();
+    void miniLaunch();
 };
 
 #endif // FIREWORK_H
